6d3c3112fdd2fa3564faba24108a24831b6b437e
varuzhantrue <varuzhan.true@gmail.com>
2022-10-07 18:46:50 +0400

    Initial commit

---
+++ src/main/java/com/capstone/cappy/CappyApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.cappy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CappyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CappyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/cappy/CappyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.cappy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CappyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
